c82747a71a198f22b0f90f25756179d8910d624f
fliu000 <fangliuche@gmail.com>
2015-12-31 16:26:18 -0500

    Update matrix_binary.m

--- matrix_binary.m
+++ matrix_binary.m
@@ -1,8 +1,8 @@
 syms D1 D2 D3 w a11 a12 a13 a21 a22 a23 a31 real
 syms deltaA A gamma1 gamma2 gamma3 real
 
-b11 = 1; b12 = 0; b14 = (1-1*i)*sqrt(D1/2/w); b15 = 0; 
-b21 = 0; b22 = 1; b24 = 0; b25 = (1-1*i)*sqrt(D2/2/w); 
+b11 = 1; b12 = 0; b14 = (1-1i)*sqrt(D1/2/w); b15 = 0; 
+b21 = 0; b22 = 1; b24 = 0; b25 = (1-1i)*sqrt(D2/2/w); 
 
 b41 = 1; b42 = 0; b44 = -a11; b45 = -a12; 
 b51 = 0; b52 = 1; b54 = -a21; b55 = -a22; 
