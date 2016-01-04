628f5f534f4ac0bdc477068248d5fb2b59214ff9
fliu000 <fangliuche@gmail.com>
2016-01-04 12:40:25 -0500

    Update matrix_binary.m

--- matrix_binary.m
+++ matrix_binary.m
@@ -1,4 +1,7 @@
+% Binary System
+
 syms D1_ D2_ a11 a12 a13 a21 a22 a23 a31 real
+% D1_ = D1 / omega; D2_ = D2 / omega;
 syms deltaA A gamma1 gamma2 gamma3 real
 
 b11 = 1; b12 = 0; b14 = (1-1i)*sqrt(2*D1_)/2; b15 = 0; 
@@ -7,7 +10,7 @@ b21 = 0; b22 = 1; b24 = 0; b25 = (1-1i)*sqrt(2*D2_)/2;
 b41 = 1; b42 = 0; b44 = -a11; b45 = -a12; 
 b51 = 0; b52 = 1; b54 = -a21; b55 = -a22; 
 
-B = [[b11,b12,b14,b15]; [b21,b22,b24,b25]; [b41,b42,b44,b45]; [b51,b52,b54,b55]]
+B = [[b11,b12,b14,b15]; [b21,b22,b24,b25]; [b41,b42,b44,b45]; [b51,b52,b54,b55]];
 
 d11 = -gamma1 * deltaA / A;
 d21 = -gamma2 * deltaA / A;
