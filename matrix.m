bb0f8ec7997ecf83fe2622080b5838b2d64351fd
fliu000 <fangliuche@gmail.com>
2016-01-04 13:36:17 -0500

    Update and rename matrix_case1.m to matrix.m

--- matrix.m
+++ matrix.m
@@ -1,4 +1,4 @@
-% Case 1: One minor component equilibrates very fast, i.e. k3 is small, deltaCs3 = 0.
+% Case 2:
 
 syms D1_ D2_ a11 a12 a21 a22 real
 % D1_ = D1 / omega; D2_ = D2 / omega;
@@ -10,7 +10,7 @@ b31 = 0; b32 = 0; b33 = 1; b34 = 0; b35 = 0;
 b41 = 1; b42 = 0; b43 = 0; b44 = -a11; b45 = -a12; 
 b51 = 0; b52 = 1; b53 = 0; b54 = -a21; b55 = -a22; 
 
-B = [[b11,b12,b13,b14,b15]; [b21,b22,b23,b24,b25]; [b31,b32,b33,b34,b35]; [b41,b42,b43,b44,b45]; [b51,b52,b53,b54,b55]];
+B = [[b11,b12,b13,b14,b15]; [b21,b22,b23,b24,b25]; [b31,b32,b33,b34,b35]; [b41,b42,b43,b44,b45]; [b51,b52,b53,b54,b55]]
 
 d11 = -gamma1 * deltaA / A;
 d21 = -gamma2 * deltaA / A;
@@ -18,13 +18,14 @@ d31 = -gamma3 * deltaA / A;
 d41 = 0;
 d51 = 0;
 
-D = [d11; d21; d31; d41; d51];
 
-F =  B \ D;
+D = [d11; d21; d31; d41; d51]
+
+F = B \ D;
 
 deltaG1 = F(1)
-% x = simplify(deltaG1)
+%x = simplify(deltaG1)
 deltaG2 = F(2)
-% y = simplify(deltaG2)
+%y = simplify(deltaG2)
 deltaG3 = F(3)
-% z = simplify(deltaG3)
+%z = simplify(deltaG3)
